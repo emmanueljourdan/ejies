a00e64c414d747592796b0513c6821408c68dfe4
jourdan <jourdan>
2006-02-21 15:51:09 +0000

    ops oublies dans le commit...

---
+++ ejies-java/ej/ListPow.java
@@ -0,0 +1,41 @@
+/*
+ * class called by ej.lop
+ * lucky me, this is automatically generated :-)
+ *
+ */
+
+package ej;
+
+public class ListPow implements ListOperator {
+
+	public float[] operate(float a, float b[])
+	{
+		float[] resultat = new float[b.length];
+		
+		for (int i = 0; i < b.length; i++)
+			resultat[i] = (float) Math.pow(a, b[i]);
+		
+		return resultat;
+	}
+
+	public float[] operate(float a[], float b)
+	{
+		float[] resultat = new float[a.length];
+		
+		for (int i = 0; i < a.length; i++)
+			resultat[i] = (float) Math.pow(a[i], b);
+		
+		return resultat;
+	}
+
+	public float[] operate(float a[], float b[])
+	{
+		int listLength = Math.min(a.length, b.length);
+		float resultat[] = new float[listLength];
+		
+		for (int i = 0; i < listLength; i++)
+			resultat[i] = (float) Math.pow(a[i], b[i]);
+		
+		return resultat;
+	}
+}
