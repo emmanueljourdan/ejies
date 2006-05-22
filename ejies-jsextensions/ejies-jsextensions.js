43d6313d22e7d8c6dda355fe93a103154f34f152
jourdan <jourdan>
2006-05-22 14:45:05 +0000

    Compatibilité MaxMSP 4.6

--- ejies-jsextensions/ejies-jsextensions.js
+++ ejies-jsextensions/ejies-jsextensions.js
@@ -10,7 +10,7 @@ post("\n     by Emmanuel Jourdan\, Ircam\n");
 
 function EjiesUtils()
 { 
-	this.VersNum = "1.56b7";		// Version Number
+	this.VersNum = "1.56b8";		// Version Number
 	this.VersDate = "(05/2006)";	// Version release date
 
 	// clip method
