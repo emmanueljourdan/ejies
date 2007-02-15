753a5f08b40eb60cbfd3b0a0290c2138a154dd3e
jourdan <jourdan>
2007-02-15 15:51:15 +0000

    ready to b3

--- ejies-jsextensions/ejies-jsextensions.js
+++ ejies-jsextensions/ejies-jsextensions.js
@@ -10,7 +10,7 @@ post("\n     by Emmanuel Jourdan\, Ircam\n");
 
 function EjiesUtils()
 { 
-	this.VersNum = "1.57b2";		// Version Number
+	this.VersNum = "1.57b3";		// Version Number
 	this.VersDate = "(02/2007)";	// Version release date
 
 	// clip method
