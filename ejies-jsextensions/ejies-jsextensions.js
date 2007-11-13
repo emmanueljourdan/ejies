abee74a366539d25ae381faaabbf79fc6b242219
jourdan <jourdan>
2007-11-13 11:21:44 +0000

    ready to 1.58b1

--- ejies-jsextensions/ejies-jsextensions.js
+++ ejies-jsextensions/ejies-jsextensions.js
@@ -10,8 +10,8 @@ post("\n     by Emmanuel Jourdan\, Ircam\n");
 
 function EjiesUtils()
 { 
-	this.VersNum = "1.57";		// Version Number
-	this.VersDate = "(06/2007)";	// Version release date
+	this.VersNum = "1.58b1";		// Version Number
+	this.VersDate = "(11/2007)";	// Version release date
 
 	// clip method
 	this.clip = function(x, min, max)
