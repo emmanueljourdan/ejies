6d6766e102b33ce84b2b6761e9ff0b1230c2b187
jourdan <jourdan>
2008-01-28 13:21:04 +0000

    *** empty log message ***

--- ejies-jsextensions/ejies-jsextensions.js
+++ ejies-jsextensions/ejies-jsextensions.js
@@ -10,8 +10,8 @@ post("\n     by Emmanuel Jourdan\, Ircam\n");
 
 function EjiesUtils()
 { 
-	this.VersNum = "1.58b1";		// Version Number
-	this.VersDate = "(11/2007)";	// Version release date
+	this.VersNum = "1.58b2";		// Version Number
+	this.VersDate = "(01/2008)";	// Version release date
 
 	// clip method
 	this.clip = function(x, min, max)
