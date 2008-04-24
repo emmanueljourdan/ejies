2b6ed8706f287e8df1a3d569c52a04452dbb0157
jourdan <jourdan>
2008-04-24 16:39:28 +0000

    plréliminaire

--- ejies-jsextensions/ejies-jsextensions.js
+++ ejies-jsextensions/ejies-jsextensions.js
@@ -10,8 +10,8 @@ post("\n     by Emmanuel Jourdan\, Ircam\n");
 
 function EjiesUtils()
 { 
-	this.VersNum = "1.58";		// Version Number
-	this.VersDate = "(03/2008)";	// Version release date
+	this.VersNum = "2.0b1";		// Version Number
+	this.VersDate = "(04/2008)";	// Version release date
 
 	// clip method
 	this.clip = function(x, min, max)
@@ -41,7 +41,6 @@ function EjiesUtils()
 
 	return this;
 }
-
 
 
 /*
