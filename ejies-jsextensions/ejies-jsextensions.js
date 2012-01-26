015fc040b6cfce65746db48583a77d659deaa934
Emmanuel Jourdan <git@e--j.com>
2012-01-26 17:40:02 +0100

    updating version number and read me

--- ejies-jsextensions/ejies-jsextensions.js
+++ ejies-jsextensions/ejies-jsextensions.js
@@ -10,8 +10,8 @@ post("\n     by Emmanuel Jourdan\, Ircam\n");
 
 function EjiesUtils()
 { 
-	this.VersNum = "3.0b1";			// Version Number
-	this.VersDate = "(01/2012)";	// Version release date
+	this.VersNum = "3.0b2";			// Version Number
+	this.VersDate = "(02/2012)";	// Version release date
 
 	// clip method
 	this.clip = function(x, min, max)
