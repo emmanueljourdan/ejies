6195e3682dfd81495c3e8844f1d304da3d7b2d00
Emmanuel Jourdan <emmanuel@cycling74.com>
2013-02-18 08:54:39 -0500

    package now grab things from git, copy everything properly for package format (see #10)

--- package-ejies.bash
+++ package-ejies.bash
@@ -2,33 +2,30 @@
 
 _root="$PWD"
 
-mkdir -p ejies_tmp
-cd  ejies_tmp
-_dst="$PWD"
-
-
 # clear
 if [ $# == 2 ] ; then
 	echo -ne "Starting Packaging of ejies version $1.$2\n"
 	
-	echo -ne "Getting source... "
-	cvs -Q export -r ejies_$1-$2 -d ejies_$1.$2 ejies
-	echo -ne "done\n"
+	echo -ne "Copying folders:\n"
+	git checkout-index -f -a --prefix=ejies_tmp/ejies_$1.$2/
+	
+	cd ejies_tmp
+	_dst="$PWD"
 	
 	echo -ne "Creating jar..."
-	cd "$_dst"/ejies_$1.$2/ejies-java
+	cd "$_dst"/ejies_$1.$2/java-classes
 	jar -cf ej.jar ej/*.class ej/*.java
 	echo -ne "done\n"
 	rm -Rf ej
 	
 	echo -ne "Copie de javadoc (I hope it's up to date)... "
 	cd "$_root"
-	cp -R ejies-java/ejies-javadoc "$_dst"/ejies_$1.$2/
+	cp -R java-classes/ejies-javadoc "$_dst"/ejies_$1.$2/java-doc
 	echo -ne "done\n"
 	
-	echo -ne "Removing bash... "
+	echo -ne "Removing unnecessary files... "
 	cd "$_dst"/ejies_$1.$2/
-	rm -f *.bash
+	rm -f *.bash README.md
 	echo -ne "done\n"	
 	
 	echo -ne "Making zip on the Desktop... "
@@ -37,16 +34,14 @@ if [ $# == 2 ] ; then
 	echo -ne "done\n"
 	mv ejies*.zip ~/Desktop
 	
-	
 	echo -ne "Removing ejies_tmp... "
 	cd "$_root"
 	rm -Rf ejies_tmp
 	echo -ne "done\n"	
-#	../install-ejies.bash ejies_$1.$2
 
 	echo "build finished: ejies_$1.$2.zip is on the ~/Desktop"
 	open ~/Desktop
 	exit 0
 fi
-echo -ne "Usage: folder to install missing...\n./package-ejies.bash /path/to/some/folder/where/you/want/the/archive/to/be/stored\n\n"
+echo -ne "Usage: Version number missing...\n./package-ejies.bash 3 03b74\n\n"
 
