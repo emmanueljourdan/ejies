083064868a64172fac6ea74379b38f77c02fe483
Emmanuel Jourdan <emmanuel.jourdan@ircam.fr>
2015-09-05 09:43:57 +0200

    removing graphics folder from the package

--- package-ejies.bash
+++ package-ejies.bash
@@ -5,33 +5,33 @@ _root="$PWD"
 # clear
 if [ $# == 2 ] ; then
 	echo -ne "Starting Packaging of ejies version $1.$2\n"
-	
+
 	echo -ne "Copying folders:\n"
 	git checkout-index -f -a --prefix=ejies_tmp/ejies_$1.$2/
-	
+
 	cd ejies_tmp
 	_dst="$PWD"
-	
+
 	echo -ne "Copie de javadoc (I hope it's up to date)... "
 	cd "$_root"
 	cp -R java-classes/ejies-javadoc "$_dst"/ejies_$1.$2/java-doc
 	echo -ne "done\n"
-	
+
 	echo -ne "Removing unnecessary files... "
 	cd "$_dst"/ejies_$1.$2/
-	rm -f *.bash README.md
-	echo -ne "done\n"	
-	
+	rm -fR *.bash README.md graphics
+	echo -ne "done\n"
+
 	echo -ne "Making zip on the Desktop... "
 	cd "$_dst"
 	zip -qrX "ejies_$1.$2.zip" "ejies_$1.$2"
 	echo -ne "done\n"
 	mv ejies*.zip ~/Desktop
-	
+
 	echo -ne "Removing ejies_tmp... "
 	cd "$_root"
 	rm -Rf ejies_tmp
-	echo -ne "done\n"	
+	echo -ne "done\n"
 
 	echo "build finished: ejies_$1.$2.zip is on the ~/Desktop"
 	open ~/Desktop
