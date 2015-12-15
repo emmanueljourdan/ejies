ad1b21a6e8005102ed8f7801d357f1cdb38f69d3
Emmanuel Jourdan <emmanuel.jourdan@ircam.fr>
2015-12-15 15:07:37 +0100

    updated bash script to just write a folder, no zip.

--- package-ejies.bash
+++ package-ejies.bash
@@ -1,41 +1,42 @@
 #!/bin/bash
 
-_root="$PWD"
+_src="$PWD"
 
 # clear
-if [ $# == 2 ] ; then
-	echo -ne "Starting Packaging of ejies version $1.$2\n"
+if [ $# == 1 ] ; then
+	_dst=~/Desktop
+	_full_dst=~/Desktop/ejies_$1/
+
+	echo -ne "Starting Packaging of ejies version $1\n"
 
 	echo -ne "Copying folders:\n"
-	git checkout-index -f -a --prefix=ejies_tmp/ejies_$1.$2/
-
-	cd ejies_tmp
-	_dst="$PWD"
+	git checkout-index -f -a --prefix="$_full_dst"
 
 	echo -ne "Copie de javadoc (I hope it's up to date)... "
-	cd "$_root"
-	cp -R java-classes/ejies-javadoc "$_dst"/ejies_$1.$2/java-doc
+	cd "$_src"
+	cp -R java-classes/ejies-javadoc "$_full_dst"/java-doc
 	echo -ne "done\n"
 
 	echo -ne "Removing unnecessary files... "
-	cd "$_dst"/ejies_$1.$2/
-	mv README.md readme.md
+	cd "$_full_dst"
+	rm -fR *.bash graphics
 	echo -ne "done\n"
 
-	echo -ne "Making zip on the Desktop... "
-	cd "$_dst"
-	zip -qrX "ejies_$1.$2.zip" "ejies_$1.$2"
-	echo -ne "done\n"
-	mv ejies*.zip ~/Desktop
+	# no longer make a zip file
+	# echo -ne "Making zip on the Desktop... "
+	# cd "$_dst"
+	# zip -qrX "ejies_$1.zip" "ejies_$1"
+	# echo -ne "done\n"
+	# mv ejies*.zip ~/Desktop
 
-	echo -ne "Removing ejies_tmp... "
-	cd "$_root"
-	rm -Rf ejies_tmp
-	echo -ne "done\n"
+	# echo -ne "Removing ejies_tmp... "
+	# cd "$_src"
+	# rm -Rf ejies_tmp
+	# echo -ne "done\n"
 
-	echo "build finished: ejies_$1.$2.zip is on the ~/Desktop"
-	open ~/Desktop
+	echo "build finished!"
+	open "$_full_dst"
 	exit 0
 fi
-echo -ne "Usage: Version number missing...\n./package-ejies.bash 3 03b74\n\n"
+echo -ne "Usage: Version number missing...\n./package-ejies.bash 3.03b74\n\n"
 
